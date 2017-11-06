9e1e49d0701b55d134b481e7efb76267a2976b2a
孙维松 <480582562@qq.com>
2017-11-06 15:44:22 +0800

    change GameWebSocket

--- src/com/ricky/socket/WsSocketServer.java
+++ src/com/ricky/socket/WsSocketServer.java
@@ -1,14 +1,25 @@
 package com.ricky.socket;
 
+import com.ricky.model.Message;
+
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ServerEndpoint("/game_socket")
 public class WsSocketServer {
+    private static String waitPlayer = null;
+    private static Map<String, Session> sendMap = new HashMap<>();
+
+    private String player;
 
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println("Open Connection..");
+        System.out.println("Server connection...");
     }
 
     @OnClose
