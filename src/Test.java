05b6c05b173fd131d78de4aa019437435827943f
孙维松 <480582562@qq.com>
2017-11-15 14:25:16 +0800

    Update Message class

--- src/Test.java
+++ src/Test.java
@@ -1,20 +1,18 @@
-import com.org.json.JSONArray;
-import com.org.json.JSONObject;
-import com.ricky.model.GMessage;
-import com.ricky.model.Player;
-import com.ricky.service.GHService;
-import com.ricky.service.UserService;
+import com.ricky.model.Message;
 
-import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args)
     {
-        GMessage message = new GMessage("message", GMessage.GType.START);
-        JSONObject jsonObj = message.toJSON();
-        System.out.println(jsonObj.toString());
-
-        GMessage mess = new GMessage(jsonObj);
-        System.out.println(mess.toString());
+        List<Integer> deck = new ArrayList<>();
+        deck.add(1);
+        deck.add(2);
+        deck.add(3);
+        Message message = new Message(Message.Type.DECK, "Ricky", deck);
+        String str = message.toJSON();
+        Message next = new Message(str);
+        System.out.println(next.toJSON());
     }
 }
--- src/com/ricky/servlet/GetUserServlet.java
+++ src/com/ricky/servlet/GetUserServlet.java
@@ -3,6 +3,7 @@ package com.ricky.servlet;
 import com.org.json.JSONObject;
 import com.ricky.model.Player;
 import com.ricky.service.UserService;
+import com.ricky.util.ModelUri;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,12 +25,13 @@ public class GetUserServlet extends HttpServlet {
         resp.setCharacterEncoding("utf-8");
         if (player != null) {
             JSONObject jsonObj = new JSONObject();
-            jsonObj.put("userID", player.getUserID());
-            jsonObj.put("username", player.getUserName());
-            jsonObj.put("password", player.getPassword());
-            jsonObj.put("question", player.getQuestion());
-            jsonObj.put("answer", player.getAnswer());
-            jsonObj.put("datetime", player.getDate() + " " + player.getTime());
+            jsonObj.put(ModelUri.USER_ID, player.getUserID());
+            jsonObj.put(ModelUri.USER_NAME, player.getUserName());
+            jsonObj.put(ModelUri.PASSWORD, player.getPassword());
+            jsonObj.put(ModelUri.QUESTION, player.getQuestion());
+            jsonObj.put(ModelUri.ANSWER, player.getAnswer());
+            jsonObj.put(ModelUri.DATE, player.getDate());
+            jsonObj.put(ModelUri.TIME, player.getTime());
 
             resp.getWriter().println(jsonObj.toString());
         }
